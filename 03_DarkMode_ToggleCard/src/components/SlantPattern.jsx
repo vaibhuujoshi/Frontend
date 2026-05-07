4efc95538bae0fcad90545af31bbb96171efeddd
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-07 19:38:41 +0530

    Fix: for dark mode

--- 03_DarkMode_ToggleCard/src/components/SlantPattern.jsx
+++ 03_DarkMode_ToggleCard/src/components/SlantPattern.jsx
@@ -1,5 +1,5 @@
 export function SlantPattern() {
     return (
-        <div className="absolute inset-0 z-0 rounded-lg m-auto border border-foreground bg-[repeating-linear-gradient(315deg,var(--pattern-color)_0,var(--pattern-color)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed"></div>
+        <div className="absolute inset-0 z-0 rounded-lg m-auto border border-foreground dark:border-neutral-800 dark:bg-[repeating-linear-gradient(315deg,var(--pattern-darkColor)_0,var(--pattern-color)_1px,transparent_0,transparent_50%)] bg-[repeating-linear-gradient(315deg,var(--pattern-color)_0,var(--pattern-color)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] dark:bg-size-[15px_15px] bg-fixed"></div>
     )
 }
