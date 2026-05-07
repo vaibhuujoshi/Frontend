0230de7416dc53a11994cf64720304ff3f3b2e6a
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-07 19:40:37 +0530

    Add: import card component

--- 03_DarkMode_ToggleCard/src/Page.jsx
+++ 03_DarkMode_ToggleCard/src/Page.jsx
@@ -1,9 +1,11 @@
+import { Card } from "./components/Card";
 import { SlantPattern } from "./components/SlantPattern";
 
 export function Home() {
     return (
-        <div className="relative h-full w-full">
+        <div className="relative h-full w-full flex items-center justify-center">
             <SlantPattern />
+            <Card />
         </div>
     )
 }
